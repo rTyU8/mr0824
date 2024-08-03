f99f0cf96814695d3e13608a40b532bd708fd394
Matthew <mwr645@gmail.com>
2024-08-03 16:38:39 -0500

    more work on rental agreement and app

--- src/main/CheckoutApplication.java
+++ src/main/CheckoutApplication.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.util.Date;
 import java.util.Scanner;
 
 import main.model.CheckoutState;
@@ -10,62 +11,67 @@ public class CheckoutApplication {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         CheckoutState checkoutState = CheckoutState.GetToolCode;
-        RentalAgreement agreement = new RentalAgreement();
         ToolRepository repository = new ToolRepository();
-
+        
+        String toolCode;
+        int rentalDayCount;
+        int discountPercent;
+        Date checkoutDate;
+        boolean interactive = true;
+        
         System.out.println("Checkout an item here (type 'exit' to quit):");
 
-        while (true) {
+        while (interactive) {
             String input;
 
             switch (checkoutState) {
                 case GetToolCode:
                     System.out.print("Enter tool code: ");
                     input = scanner.nextLine();
+                    validateTool
+                    checkoutState = CheckoutState.GetRentalDayCount;
                     break;
                 case GetRentalDayCount:
                     System.out.print("Enter the number of days you want to rent the tool: ");
                     input = scanner.nextLine();
+                    checkoutState = CheckoutState.GetDiscountPercent;
                     break;
                 case GetDiscountPercent:
                     System.out.print("Enter the discount percentage (whole number between 0-100): ");
                     input = scanner.nextLine();
+                    checkoutState = CheckoutState.GetCheckoutDate;
                     break;
                 case GetCheckoutDate:
                     System.out.print("Enter the checkout date (in mm/dd/yyyy format): ");
                     input = scanner.nextLine();
+                    checkoutState = CheckoutState.GenerateRentalAgreement;
                     break;
                 case GenerateRentalAgreement:
-                    System.out.print("Enter the checkout date (in mm/dd/yyyy format): ");
-                    input = scanner.nextLine();
+                	interactive = false;
                     break;
                 default:
+                	System.out.println("Unreachable..");
+                	interactive = false;
                     break;
             }
 
-            if (input.equalsIgnoreCase("exit")) {
-                break;
-            }
-
-            System.out.println("You entered: " + input);
         }
 
-        System.out.println("Goodbye.");
         scanner.close();
     }
 
     private void printCheckoutTextPrompt(CheckoutState state) {
-        switch (checkoutState) {
-            case CheckoutState.GetToolCode:
+        switch (state) {
+            case GetToolCode:
                 System.out.print("Enter tool code: ");
                 break;
-            case CheckoutState.GetRentalDayCount:
+            case GetRentalDayCount:
                 System.out.print("Enter the number of days you want to rent the tool: ");
                 break;
-            case CheckoutState.GetDiscountPercent:
+            case GetDiscountPercent:
                 System.out.print("Enter the discount percentage (whole number between 0-100): ");
                 break;
-            case CheckoutState.GetCheckoutDate:
+            case GetCheckoutDate:
                 System.out.print("Enter the checkout date (in mm/dd/yyyy format): ");
                 break;
             default:
--- src/main/model/RentalAgreement.java
+++ src/main/model/RentalAgreement.java
@@ -1,14 +1,98 @@
 package main.model;
 
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class RentalAgreement {
 
-    private String brand;
-
+    private Tool tool;
+    
+    private ChargeTableEntry chargeDetails;
+    
+    private int rentalDays;
+    
+    private int chargeDays;
+    
+    private Date checkoutDate;
+    
+    private Date dueDate;
+    
     private double dailyCharge;
+    
+    private double preDiscountCharge;
+    
+    private int discountPercent;
+    
+    private double discountAmount;
+    
+    private double finalCharge;
+    
+    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("MM/dd/yy");
 
-    private boolean weekdayCharge;
+    private static final NumberFormat CURRENCY_FORMAT = NumberFormat.getCurrencyInstance(Locale.US);
 
-    private boolean weekendCharge;
+    private static final NumberFormat PERCENT_FORMAT = NumberFormat.getPercentInstance();
 
-    private boolean holidayCharge;
+    // Set the maximum number of fraction digits to 2 (optional)
+
+	public RentalAgreement(Tool tool, int rentalDays, int discountPercent, Date checkout) {
+		this.tool = tool;
+		this.rentalDays = rentalDays;
+		this.discountPercent = discountPercent;
+		this.checkoutDate = checkout;
+	    PERCENT_FORMAT.setMaximumFractionDigits(0);
+	}
+    
+    // Getter and Setter for tool
+    public Tool getTool() {
+        return tool;
+    }
+
+    public ChargeTableEntry getChargeDetails() {
+        return chargeDetails;
+    }
+
+    public int getRentalDays() {
+        return rentalDays;
+    }
+
+    public Date getCheckoutDate() {
+        return checkoutDate;
+    }
+    public double getDailyCharge() {
+        return dailyCharge;
+    }
+
+    public double getPreDiscountCharge() {
+        return preDiscountCharge;
+    }
+
+    public int getDiscountPercent() {
+        return discountPercent;
+    }
+
+    public double getDiscountAmount() {
+        return discountAmount;
+    }
+
+    public double getFinalCharge() {
+        return finalCharge;
+    }
+    
+    public String toString() {
+    	return "Tool code: " + tool.getCode() +
+    			"\nTool type: " + tool.getToolType() +
+    			"\nTool brand: " + tool.getBrand() +
+                "\nRental Days: " + rentalDays +
+                "\nCheck out Date: " + DATE_FORMAT.format(checkoutDate) +
+                "\nDue Date: " + DATE_FORMAT.format(dueDate) +
+                "\nDaily Rental Charge: " + CURRENCY_FORMAT.format(dailyCharge) +
+                "\nCharge Days: " + chargeDays +
+                "\nPre-Discount Charge: " + CURRENCY_FORMAT.format(preDiscountCharge) +
+                "\nDiscount Percent: " + PERCENT_FORMAT.format(discountPercent) +
+                "\nDiscount Amount: " + CURRENCY_FORMAT.format(discountAmount) +
+                "\nFinal Charge: " + CURRENCY_FORMAT.format(finalCharge);
+    }
 }
